541845c57f74113ec42836fa13ab71ce7a2656c3
José Rafael Becerra <jose.rabelo95@gmail.com>
2018-12-12 12:51:02 -0600

    Update cries.asm

--- audio/cries.asm
+++ audio/cries.asm
@@ -3,3 +3,4 @@ INCLUDE "constants.asm"
 SECTION "Cries", ROMX[$51CB], BANK[$3C] ; TODO: Temporary, please map 4000-51CA and move this section's base
 
 CryHeaders:: ; TODO: Rip the data, then INCBIN it
+##Hello this is just a random comment :) 
